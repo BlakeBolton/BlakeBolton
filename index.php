44704931992c61a969681ee1286765de0a1dd148
Blake Bolton <148376891+BlakeBolton@users.noreply.github.com>
2024-02-15 11:07:29 -0600

    testing commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php
     while(have_posts()) {
         the_post(); ?>
-
+        <p>TESTING!</p>
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php the_content(); ?>
         <hr>
